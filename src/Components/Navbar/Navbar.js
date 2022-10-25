5d62bb58c397cdab6fb7905aff45bbf42bb07068
NowrinIslamMim <nowrin02@gmail.com>
2022-10-25 22:38:08 +0600

    testimony component added

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -102,7 +102,7 @@ const Navbar = () => {
                                 <li>
                                     <Link
                                         to="/register"
-                                        className="inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
+                                        className="inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-red-500 hover:bg-red-600 focus:shadow-outline focus:outline-none"
                                         aria-label="Sign up"
                                         title="Register"
                                     >
